6322589822f603bc860ba52eb360530bc5d5b17f
Carla Romero <carlagromero@gmail.com>
2019-04-23 15:14:59 -0300

    Updates

---
+++ precache-manifest.8ee699476d199b7eec43cd89fc699cd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfb3f6458260d8b3727628634c2b2d45",
+    "url": "/cryptocurrencyghpages/index.html"
+  },
+  {
+    "revision": "77a31a2cb827b8c10306",
+    "url": "/cryptocurrencyghpages/static/css/main.398c73e6.chunk.css"
+  },
+  {
+    "revision": "8a161f5ae0b4ea5a0835",
+    "url": "/cryptocurrencyghpages/static/js/2.80c8ecd3.chunk.js"
+  },
+  {
+    "revision": "77a31a2cb827b8c10306",
+    "url": "/cryptocurrencyghpages/static/js/main.8eeb3d0d.chunk.js"
+  },
+  {
+    "revision": "721e6aeb07fec253d4e0",
+    "url": "/cryptocurrencyghpages/static/js/runtime~main.51925b4b.js"
+  },
+  {
+    "revision": "96e45d694833f161c9dc02ab9dde4a6b",
+    "url": "/cryptocurrencyghpages/static/media/cryptocurrencies.96e45d69.png"
+  }
+]);
